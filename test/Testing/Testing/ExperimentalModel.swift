572464c170f3111c1e4a1b631ee9555f116839da
Pamela Needle <Pamneedle@Pamelas-MacBook-Pro.local>
2015-08-11 17:50:52 +1000

    changes

---
+++ test/Testing/Testing/ExperimentalModel.swift
@@ -0,0 +1,13 @@
+//
+//  ExperimentalModel.swift
+//  Testing
+//
+//  Created by Pamela Needle on 8/11/15.
+//  Copyright (c) 2015 Pamela Needle. All rights reserved.
+//
+
+import UIKit
+
+class ExperimentalModel: NSObject {
+   
+}
