d7034f74bae4c5e6cdc9425c7d6dd627d16de57e
Pamela Needle <Pamneedle@Pamelas-MacBook-Pro.local>
2015-08-17 19:28:53 +1000

    compelted part3- FINALLY

--- Assignment1/The Memory Game/TileView.swift
+++ Assignment1/The Memory Game/TileView.swift
@@ -56,38 +56,11 @@ class TileView: UIView{
             attribute: NSLayoutAttribute.Left,
             multiplier: 1,
             constant: 0)
-        self.addConstraints([widthConstraint, heightConstraint, topConstraint, leftConstraint])
         self.addSubview(imageView)
+        self.addConstraints([widthConstraint, heightConstraint, topConstraint, leftConstraint])
         
-        //self.img = UIImage(named: "lake")!
-        
-        imageView.contentMode = UIViewContentMode.ScaleToFill
-        //super.init(coder: aDecoder)
+       
     }
-    
-    /*init(){
-        self.tileIndex = self.tag
-        imageView = UIImageView(frame:CGRectMake(0, 0, 100, 100))
-        imageView.image = UIImage(named: "question")
-        self.img = UIImage(named: "lake")!
-        ///self.view.addSubview(imageView)
-        imageView.contentMode = UIViewContentMode.ScaleToFill
-        
-        //super.init(frame: CGRect(x: 0,y: 0,width: 100,height: 100))
-        //self.userInteractionEnabled = true
-        
-    }*/
-
-
-
-    /*
-    //self.view.viewWithTag(i)
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }*/
-    
-
     
     func revealImage(){
         imageView.image = img
@@ -100,5 +73,11 @@ class TileView: UIView{
     func hideTiles(){
         imageView.alpha = 0
     }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        revealImage()
+    }
+    
+    
     
 }
